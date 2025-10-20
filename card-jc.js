c1f663fc3a91046774277c708c74de9eb6d6cf0b
Moisés Falcão <9012822+moisesfalcao@users.noreply.github.com>
2025-10-20 19:21:49 -0300

    Update card-jc.js

--- card-jc.js
+++ card-jc.js
@@ -44,7 +44,7 @@
     headerBox: {
       background: "linear-gradient(to bottom,  rgba(0,0,0,0) 0%,rgba(0,0,0,1) 100%)",
       radius: 0,
-      padding: "3rem",
+      padding: "10rem 5rem 5rem 5rem",
       textColor: "#fff",
       borderBottom: "10px solid #e01e24;",
     },
